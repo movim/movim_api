93dc5689b7b3e6777b4b07d4d7ccc97e8e9470bd
Timothée Jaussoin <edhelas@movim.eu>
2016-11-23 23:24:34 +0100

    Add space between the button and paragraph in accounts.created

--- resources/views/accounts/created.blade.php
+++ resources/views/accounts/created.blade.php
@@ -9,6 +9,7 @@
     <p>You can now login on your favorite client</p>
 
     @if (!empty($referer))
+    <br />
     <a class="button color" href="{{ parse_url($referer)['scheme'] }}://{{ parse_url($referer)['host'] }}">
         Go back to {{ parse_url($referer)['host'] }}
     </a>
