e216bdb18a4e32fa9bcecb714d9a553529e38b26
Timothée Jaussoin <edhelas@movim.eu>
2016-11-23 23:19:35 +0100

    Link to the root of the referer

--- resources/views/accounts/created.blade.php
+++ resources/views/accounts/created.blade.php
@@ -9,7 +9,7 @@
     <p>You can now login on your favorite client</p>
 
     @if (!empty($referer))
-    <a class="button color" href="{{ $referer }}">
+    <a class="button color" href="{{ parse_url($referer)['scheme'] }}://{{ parse_url($referer)['host'] }}">
         Go back to {{ parse_url($referer)['host'] }}
     </a>
     @endif
