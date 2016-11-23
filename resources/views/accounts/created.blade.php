ad9ca56df2b886dfe6d4aa425462f05b6088c327
Timothée Jaussoin <edhelas@movim.eu>
2016-11-23 23:01:15 +0100

    Simplifiy the registration sentence

--- resources/views/accounts/created.blade.php
+++ resources/views/accounts/created.blade.php
@@ -6,7 +6,7 @@
 
     <h2>{{ $jid }}</h2>
 
-    <p>You can now login with it on your favorite client</p>
+    <p>You can now login on your favorite client</p>
 
     @if (!empty($referer))
     <a class="button color" href="{{ $referer }}">
