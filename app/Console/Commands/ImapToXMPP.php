c46322b434791dfeeb8476b7fa2e0fb396b01d0a
Timothée Jaussoin <edhelas@movim.eu>
2020-02-14 20:05:27 +0100

    Add from

--- app/Console/Commands/ImapToXMPP.php
+++ app/Console/Commands/ImapToXMPP.php
@@ -60,7 +60,7 @@ class ImapToXMPP extends Command
 
             foreach ($mailsIds as $mailsId) {
                 $mail = $mailbox->getMail($mailsId, true);
-
+dd($mail);
                 $tos = array_merge(array_keys($mail->to), array_keys($mail->cc), array_keys($mail->bcc));
                 $extractedTo = false;
                 foreach($tos as $to) {
@@ -79,7 +79,7 @@ class ImapToXMPP extends Command
                             'from' => config('imaptoxmpp.xmpp_from'),
                             'to' => $extractedTo,
                             'subject' => $mail->subject,
-                            'body' => '📥 New email received: "'.$mail->subject.'"'
+                            'body' => '📥 New email from '.$mail->fromAddress.': "'.$mail->subject.'"'
                         ]
                     ]);
 
