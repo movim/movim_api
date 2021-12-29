2f53c7b86c26b0582ff8c4b5bf01ebb2bfbdef2e
Timothée Jaussoin <edhelas@movim.eu>
2021-12-30 00:11:58 +0100

    Update the legals Update the dependencies

--- resources/views/accounts/legals.blade.php
+++ resources/views/accounts/legals.blade.php
@@ -30,6 +30,21 @@
         <li>The mov.im Movim instance</li>
     </ul>
 
+    <h4>Hosted in France in a personnal self-hosted server</h4>
+    <ul>
+        <li>The XMPP services and Movim instance database backups</li>
+    </ul>
+
+    <h1>About</h1>
+
+    <p>All our services are offered for free.</p>
+    <p>We are hosting them to the Libre and Open Source Softwares movement and to offer non-commercial alternative solutions to the centralized and commercial messaging and social platforms.</p>
+
+    <h1 id="laws">Laws</h1>
+
+    <p>Our services are stored in Germany and France. For the correspondig data hosted in each country, the French and German juridiction applies.</p>
+    <p>All our services are hosted in the European Union, therefore the <a href="https://en.wikipedia.org/wiki/General_Data_Protection_Regulation">GDPR</a> applies to all the data we are managing.</p>
+
     <h1 id="privacy_policy">Privacy Policy</h1>
 
     <h2 id="data_policy">What we store</h2>
@@ -38,12 +53,14 @@
     <ul>
         <li>Your username, a hash of your password and related data (vcard, profile picture)</li>
         <li>Your email, if provided, to allow you to recover your password</li>
-        <li><b>Articles</b> All the articles published by you on our services</li>
-        <li><b>Messages</b> All the messages published and received by you or your contacts or from the chatrooms you are connected on</li>
-        <li><b>Files</b> All the pictures uploaded using your account</li>
+        <li><b>Articles</b> All the articles and comments published by you on our services</li>
+        <li><b>Messages</b> All the messages published and received by you or your contacts or from the chatrooms you are connected on. You can configure messages storage using the Movim configuration panel.</li>
+        <li><b>Files</b> All the pictures and files uploaded using your account.</li>
         <li><b>Contacts</b> Your contact list (XMPP Roster)</li>
         <li>All the data published by Movim or other XMPP clients you are using on your XMPP account (OMEMO public keys, bookmarks.)</li>
     </ul>
+
+    <p>All the data above are store indefinitely on our services until you destroy your account, see <a href="#withdrawal">Withdrawal</a>.</p>
 
     <p>For XMPP accounts using our mov.im public Movim instance:</p>
     <ul>
@@ -55,15 +72,24 @@
         <li>A cache of all the data published by Movim or other XMPP clients you are using on your XMPP account (OMEMO public keys, bookmarks.)</li>
     </ul>
 
+    <p>All the data above are stored indefinitely on our services. Using the Movim configuration panel you can choose to leave the instance. If you do so, all your XMPP account related data (including non-shared cache) will be destroyed.</p>
+
     <p>To operate our services we are also storing some logs and related data, those logs are rotated daily and destroyed using the following rules:</p>
 
     <ul>
         <li>ejabberd (our XMPP server): 14 days, errors and general info (failed connections, security errors)</li>
         <li>nginx (our Web server): 14 days, errors and pages accessed (containing your IP and browser User-Agent)</li>
-        <li>A global backup of our main databases (XMPP and Movim) is done daily on a local server in France. This backup is refreshed each night and is only used for restoration purpose</li>
+        <li>A global backup of our main databases (XMPP and Movim) is done daily on a local self-hosted server in France. This backup is refreshed each night and is only used for restoration purpose</li>
     </ul>
 
-    <h3>
+    <h2 id="data_policy">Limits</h2>
+
+    <p>Some limits are also applied to our services:</p>
+
+    <ul>
+        <li><b>File Upload</b> are limited to 10Mo per file. There is no other limit (total volume or time) regarding the storage of those files</li>
+        <li><b>XMPP and HTTP connections</b> we have some connections limitations in place, both on our XMPP services and web services to limit the bandwith and amount of requests per user</li>
+    </ul>
 
     <h1 id="terms_conditions">Terms and Conditions</h2>
 
@@ -71,8 +97,10 @@
 
     <p>By using our services, you approve to the following terms. If you do not, please remove your account and all the content you shared and stop using our services.</p>
 
+    <p>We are also giving ourself the right to remove all the data that are not complying with the sections bellow or transfer them to the authorities if required.</p>
+
     <h3>1.1. Be responsible of your acts</h3>
-    <p>You are legally responsible of everything you do on our services. This includes your messages to others, the chatrooms you create, the files you share.</p>
+    <p>You are legally responsible of everything you do on our services. This includes your messages to others, the chatrooms you create, the files you share...</p>
     <p>Chat rooms and Movim Communities owners are responsible of the ideas and the content their users share. This is their job to name moderators to keep control on what is done there.</p>
     <p>We are not responsible of your commitments. Every legal problem concerning content you shared will be assigned to you and not Movim, the owner of the service, nor netcup GMbH, our server providers.</p>
 
@@ -92,60 +120,37 @@
     <h3>1.5. Do not post injurious, racist, libelous or pedophile content</h3>
     <p>Such behavior is punished by the law. You, as a citizen, is responsible of what you say on the service.</p>
 
-    <h3>1.6. Do not try to sell accounts</h3>
+    <h3>1.6. Do not use our service for profits</h3>
     <p>You are not allowed to take profits from our service, by selling user accounts for instance. Usage of our service must remain non-profit (it can be used in companies, but no money can be done via our service).</p>
 
     <h3>1.7. Do not try to hack accounts</h3>
-    <p>Respect others, do not try to get their password.</p>
+    <p>Respect others, do not try to get their password or personnal information by any official uses.</p>
 
     <h3>1.8. Do not steal someone else's brand or name</h3>
     <p>When a brand does not belong to you, you're not allowed to use the same name for your username or chatroom name on our network. That's the same for people's identity. You're not allowed to take the identity of somebody else.</p>
 
+    <h2>2. Your rights</h2>
 
-    <h2>2. Your privacy</h2>
+    <p>Our services are hosted in the European Union, therefore the GDPR applies to all the data we manage linked to your account or activities.</p>
 
-    <p>We feel responsible, as a communication service operator, of your data safety. We ensure that every communication you can have through our platform is secured and cannot be read by third parties.</p>
-
-    <h3>2.1. We keep your data secure</h3>
-    <p>Each bit of the data we store count. Our servers are located in secured datacenters managed by TransIP (Netherlands) and OVH (France).</p>
-    <p>We use robust passwords for our services administration, so that you can feel safe. We've got local backups of the server stored on a secured drive. That's all.</p>
-
-    <h3>2.2. We don't look at your messages</h3>
-    <p>We don't have the willing to read your messages.</p>
-    <p>If you still don't feel secure, you can use e2e (end to end) encryption betweek two XMPP clients that support it. Please note that depending of the country you're living in, you may not be allowed to use that strength cryptography. Please refer to your local law texts.</p>
-
-    <h3>2.3. We don't look at your files</h3>
-    <p>Tracking the content you share is not our goal. We won't have the time for that by the way!</p>
-    <p>The only persons who can look at these files you share are your friends or the visitors of your profile. You control the spread of files.</p>
-
-    <h3>2.4. Communications are encrypted</h3>
-    <p>By default, all communications between any client to our server are encrypted.</p>
-    <p>Communications between our server and remote servers (e.g. movim.eu to jabber.org) are also secured that way.</p>
-
-    <h3>2.5. We don't sell your data to third parties</h3>
-    <p>Our platform has no commercial activity. We do not require to sell your data (private or public) in that case.</p>
-    <p>Remember we will never sell your data to third parties.</p>
-
-    <h3>2.6. We are crazy about privacy and communication safety</h3>
-    <p>We love our users, we respect them and they are our strength. Why would we compromise them?</p>
-    <p>We do all our possible to protect your safety and privacy, outside any legal issue resolution.</p>
-
-    <h2>3. Your rights</h2>
-
-    <p>We want to ensure everyone's rights are fulfilled. That's why are fast on responding to legal-related problems.</p>
-
-    <h3>3.1. Remove content concerning you</h3>
+    <h3>2.1. Remove content concerning you</h3>
     <p>This includes your account and related contents and your files stored on our system.</p>
     <p>You can also ask us to remove any content concerning you that is owned by another user on our platform.</p>
 
-    <h>3.2. Getting content concerning you</h5>
+    <h3>2.2. Getting content concerning you</h3>
     <p>You can ask us to get a copy of all the content we store concerning you.</p>
 
-    <h3>3.3. Getting data about other parties to resolve a legal issue</h3>
+    <h3>2.3. Getting data about other parties to resolve a legal issue</h3>
     <p>In order to resolve a legal issue between you and another user of the platform, you are allowed to ask us for the contact information of an user (this includes email address, phone number, postal address or IP address).</p>
     <p>We won't share data concerning somebody to anyone, you will need to prove your identity before, and only request contact data or data which can prove something useful in the issue resolution.</p>
 
-    <h2>4. Withdrawal</h2>
+    <h2>3. No Warranty</h2>
+
+    <p>You understand and agree that our services are offered as is, subject to availability and without liability to you. As much as we would not like to let any of our users down, we just can not give any warranty as to the reliability, accessibility, or quality of our services.</p>
+
+    <p>You agree that the use of our services is at your sole and exclusive risk.</p>
+
+    <h2 id="withdrawal">4. Withdrawal</h2>
 
     <p>This documentation may be updated at least one time per year. As an user, you must stay up to date in this agreement, please follow updates of this document.</p>
     <p>You own the right to retract yourself from our service if you don't agree to this document.</p>
@@ -156,7 +161,7 @@
         <li>Remove all your data on our service</li>
     </ol>
 
-    <p>We should not publish an updated document that diverges from our vision, so that you should agree with the next revisions of this document.</p>
+    <p>You can simply destroy your account and all the related data using the Movim configuration panel, Account tab, "Delete your Account" button. We might keep some account related data for some times regarding our <a href="#privacy_policy">Privacy Policy</a>.</p>
         </content>
     </section>
 </article>
